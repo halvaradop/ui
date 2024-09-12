704a1a49cc0c0f6873b6c3d38b6562ddc243982c
halvaradop <hernanvid123@gmail.com>
2024-09-12 15:23:12 -0500

    chore: update tailwindcss.config.ts file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss"
 import utilities from "@halvaradop/tailwindcss-utilities"
 
 const config: Config = {
-	content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./ui/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+	content: ["./ui/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "./packages/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
 		extend: {
 			screens: {
