541990c80d7fdf4f162cbc469a29e8e32d0b7103
halvaradop <hernanvid123@gmail.com>
2024-09-12 15:25:03 -0500

    chore: add `darkMode`and `future` properties in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,10 @@ import utilities from "@halvaradop/tailwindcss-utilities"
 
 const config: Config = {
 	content: ["./ui/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "./packages/**/*.{js,ts,jsx,tsx,mdx}"],
+	darkMode: "class",
+	future: {
+		hoverOnlyWhenSupported: true
+	},	
 	theme: {
 		extend: {
 			screens: {
