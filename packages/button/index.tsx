3b8a281ddc66b0ce82029f98dfcbd2192429b8b0
halvaradop <hernanvid123@gmail.com>
2024-09-12 19:13:37 -0500

    feat: add `Button` component and set up workspaces
    - Release version `0.1.0` of the `Button` component on npm
    - Install class-variance-authority
    - Set up workspaces for the project, including adding the workspaces
      keyword in the scripts and field in package.json
    
    - Add packages with a pattern in the content field in `tailwindcss.config.ts`

---
+++ packages/button/index.tsx
@@ -0,0 +1,45 @@
+import { forwardRef, ButtonHTMLAttributes } from "react"
+import type { ArgsFunction } from "@halvaradop/ts-utility-types"
+import { cva, type VariantProps } from "class-variance-authority"
+
+export type ButtonProps<T extends ArgsFunction> = Omit<ButtonHTMLAttributes<HTMLButtonElement>, "size"> & VariantProps<T>
+
+export const buttonVariants = cva("flex items-center justify-center border focus-visible:outline-none", {
+	variants: {
+		variant: {
+			base: "",
+			shadow: "",
+			ghost: "",
+			link: "",
+		},
+		size: {
+			sm: "h-9 px-3 text-sm rounded-md",
+			md: "h-10 px-4 text-base rounded-md",
+			lg: "h-10 px-5 text-lg rounded-md",
+			xl: "h-11 px-4 text-lg rounded-lg",
+		},
+		fullWidth: {
+			true: "w-full",
+			false: "w-fit",
+		},
+		fullRounded: {
+			true: "rounded-full",
+			false: "rounded",
+		},
+	},
+	defaultVariants: {
+		size: "md",
+		fullWidth: false,
+		fullRounded: false,
+	},
+})
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps<typeof buttonVariants>>(
+	({ className, size, fullWidth, fullRounded, children }, ref) => {
+		return (
+			<button className={buttonVariants({ className, size, fullWidth, fullRounded })} ref={ref}>
+				{children}
+			</button>
+		)
+	}
+)
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,11 @@
 import type { Config } from "tailwindcss"
-import utilities from "@halvaradop/tailwindcss-utilities"
 
 const config: Config = {
 	content: ["./ui/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "./packages/**/*.{js,ts,jsx,tsx,mdx}"],
 	darkMode: "class",
 	future: {
-		hoverOnlyWhenSupported: true
-	},	
+		hoverOnlyWhenSupported: true,
+	},
 	theme: {
 		extend: {
 			screens: {
@@ -14,6 +13,6 @@ const config: Config = {
 			},
 		},
 	},
-	plugins: [utilities],
+	plugins: [],
 }
 export default config
