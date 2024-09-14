8e61f22d0d5393ecb4937e2bfb0ae0f443a5d88c
Hernan Alvarado <hernanvid123@gmail.com>
2024-09-14 12:52:51 -0500

    chore: improve variants for button component (#7)
    * chore: improve variants for button component
    
    * chore: add classes to ghost and outline variants

--- packages/button/src/index.tsx
+++ packages/button/src/index.tsx
@@ -1,15 +1,18 @@
 import { forwardRef, ComponentProps } from "react"
 import type { ArgsFunction } from "@halvaradop/ts-utility-types"
 import { cva, type VariantProps } from "class-variance-authority"
+import { merge } from "@halvaradop/ui-core"
 
 export type ButtonProps<T extends ArgsFunction> = Omit<ComponentProps<"button">, "size"> & VariantProps<T>
 
-export const buttonVariants = cva("flex items-center justify-center border focus-visible:outline-none base:px-8", {
+export const buttonVariants = cva("flex items-center justify-center font-semibold border transition hover:border-transparent hover:bg-opacity-90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2", {
 	variants: {
 		variant: {
-			base: "text-white border-black focus-visible:ring-1 focus:ring-black",
-			ghost: "text-black border-transparent bg-transparent",
-			link: "text-black border-none bg-transparent",
+			base: "text-white border-black bg-black focus-visible:ring-black",
+			ghost: "text-black border-transparent bg-transparent hover:text-white hover:bg-black/90 focus-visible:text-white focus-visible:bg-black/90 focus-visible:ring-0 focus-visible:ring-offset-0 ",
+			link: "text-black border-none hover:underline hover:underline-offset-4 hover:decoration-black focus-visible:underline focus-visible:underline-offset-4 focus:decoration-black focus-visible:ring-0 focus-visible:ring-offset-0",
+			destructive: "text-white border-red-500 bg-red-500 focus-visible:ring-red-500",
+			outline: "text-black border-black hover:border-black focus-visible:ring-black",
 		},
 		size: {
 			sm: "h-9 px-3 text-sm rounded-md",
@@ -36,7 +39,7 @@ export const buttonVariants = cva("flex items-center justify-center border focus
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps<typeof buttonVariants>>(({ className, variant, size, fullWidth, fullRounded, children, ...props }, ref) => {
 	return (
-		<button className={buttonVariants({ className, variant, size, fullWidth, fullRounded })} ref={ref} {...props}>
+		<button className={merge(buttonVariants({ className, variant, size, fullWidth, fullRounded }))} ref={ref} role="button" {...props}>
 			{children}
 		</button>
 	)
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,14 @@
 import type { Config } from "tailwindcss"
 
+// prettier-ignore
 const config: Config = {
-	content: ["./ui/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "./packages/**/*.{js,ts,jsx,tsx,mdx}"],
+	content: [
+		"./ui/**/*.{js,ts,jsx,tsx,mdx}", 
+		"./app/**/*.{js,ts,jsx,tsx,mdx}", 
+		"./packages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./node_modules/**/*.{js,ts,jsx,tsx,mdx}",
+		"./node_modules/@halvaradop/**/*.{js,ts,jsx,tsx,mdx}",
+	],
 	darkMode: "class",
 	future: {
 		hoverOnlyWhenSupported: true,
