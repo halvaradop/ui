132d8947a0d8241c8ca0766294912608ef2c7390
halvaradop <hernanvid123@gmail.com>
2024-09-16 22:55:23 -0500

    chore: export dist files
    - Added `vite.config.base.ts` to handle exports in package.json
    
    ** Pending **
    When bundling for `ui-core`, `tailwind-merge` and `vite-plugin-dts` are
    not recognized as external dependencies. Consider implementing `Rollup`
    or `Vite` plugins to address this issuechore: export dist files
    
    - Added `vite.config.base.ts` to handle exports in package.json
    
    ** Pending **
    When bundling for `ui-core`, `tailwind-merge` and `vite-plugin-dts` are
    not recognized as external dependencies. Consider implementing `Rollup`
    or `Vite` plugins to address this issue.

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 import { defaultConfig } from "./src/vite.config.default"
 
 /**
- * Vite configuration for the @halvaradop package. This configuration uses
- * the default configuration provided by this package.
+ * Vite configuration for the @halvaradop/ui library. This configuration
+ * uses the default configuration provided by this package.
  */
 export default defineConfig(defaultConfig)
