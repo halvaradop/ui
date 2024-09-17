d0db3dfe1d098ee344bf44bdc86884c15fa76678
halvaradop <hernanvid123@gmail.com>
2024-09-16 22:39:13 -0500

    chore: install and set up vite
    - Installed `vite` and `vite-plugin-dts`
    - Added `vite.config.default.ts` for vite config
    - Changed postcss extension from `.ts` to `.js
    - Moved `index.ts` content to `utils.ts`
    - Excluded dependencies from the bundle

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,12 +1,2 @@
-import { twMerge } from "tailwind-merge"
-
-/**
- * Merge multiple classes into a single class string. It prioritizes the
- * last class in case of conflicts.
- * @params classes - The classes to merge.
- * @returns The merged class string.
- * @example
- * // Expected: "text-red-500"
- * merge("text-blue-500 text-red-500")
- */
-export const merge = twMerge
+export * from "./utils.js"
+export * from "./vite.config.default.js"
---
+++ packages/core/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite"
+import { defaultConfig } from "./src/vite.config.default"
+
+/**
+ * Vite configuration for the @halvaradop package. This configuration uses
+ * the default configuration provided by this package.
+ */
+export default defineConfig(defaultConfig)
