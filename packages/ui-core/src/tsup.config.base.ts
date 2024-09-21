c17eff537a24595b6a01ffc7129cb4dafa3b5133
Hernan Alvarado <hernanvid123@gmail.com>
2024-09-20 20:05:00 -0500

    chore: exclude `.stories.tsx` files `tsup.config.base.ts` (#19)

--- packages/ui-core/src/tsup.config.base.ts
+++ packages/ui-core/src/tsup.config.base.ts
@@ -1,11 +1,13 @@
 import { Options } from "tsup"
 
+/**
+ * Base tsup configuration for all packages.
+ */
 export const tsupConfig: Options = {
-	entry: ["src"],
+	entry: ["src", "!src/**/*.stories.tsx"],
 	format: ["esm", "cjs"],
 	dts: true,
 	clean: true,
-	splitting: false,
 	minify: true,
 	external: ["vite", "react", "react-dom", "tailwindcss"],
 }
