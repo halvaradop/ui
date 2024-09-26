61121820f6577fd974aafd909a652f57663d9393
Hernan Alvarado <hernanvid123@gmail.com>
2024-09-26 14:27:46 -0500

    feat: add ui-template package

---
+++ packages/ui-template/src/index.tsx
@@ -0,0 +1,13 @@
+import { ComponentProps } from "react"
+import type { ArgsFunction } from "@halvaradop/ts-utility-types"
+import { cva, type VariantProps } from "class-variance-authority"
+
+export type IndexProps<T extends ArgsFunction> = VariantProps<T> & ComponentProps<"div">
+
+export const indexVariants = cva("", {
+	variants: {},
+})
+
+export const Index = () => {
+	return null
+}
