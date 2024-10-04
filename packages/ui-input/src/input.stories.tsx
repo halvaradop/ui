96e6306fc656ad56d8b305a458319b3350adb184
Hernan Alvarado <hernanvid123@gmail.com>
2024-10-04 13:38:27 -0500

    feat: add `required` and `outline` variants for input

--- packages/ui-input/src/input.stories.tsx
+++ packages/ui-input/src/input.stories.tsx
@@ -12,29 +12,60 @@ const meta: Meta = {
 
 type Story = StoryObj<typeof meta>
 
-export const Base: Story = {}
+export const Base: Story = {
+	args: {
+		placeholder: "Base input",
+	},
+}
 
 export const Small: Story = {
 	args: {
 		size: "sm",
+		placeholder: "Small input",
 	},
 }
 
 export const Medium: Story = {
 	args: {
 		size: "md",
+		placeholder: "Medium input",
 	},
 }
 
 export const Large: Story = {
 	args: {
 		size: "lg",
+		placeholder: "Large input",
 	},
 }
 
 export const FullWidth: Story = {
 	args: {
 		fullWidth: true,
+		placeholder: "FullWidth input",
+	},
+}
+
+export const FullRounded: Story = {
+	args: {
+		fullRounded: true,
+		placeholder: "FullRounded input",
+	},
+}
+
+export const Outline: Story = {
+	args: {
+		size: "sm",
+		variant: "outline",
+		placeholder: "Outline input",
+	},
+}
+
+export const Required: Story = {
+	args: {
+		size: "base",
+		variant: "required",
+		placeholder: "Required input",
 	},
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss"
+import plugin from "tailwindcss/plugin"
 
 const config: Config = {
 	content: ["./ui/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "./packages/**/*.{js,ts,jsx,tsx,mdx}"],
@@ -13,7 +14,12 @@ const config: Config = {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(({ addVariant }) => {
+			addVariant("usinvalid", "&:user-invalid")
+			addVariant("usvalid", "&:user-valid")
+		}),
+	],
 }
 
 export default config
