ea3f07173acc5f11dbd4cd76f670136446f1cf5e
Hernan Alvarado <hernanvid123@gmail.com>
2024-10-28 16:52:33 -0500

    feat: add `disabled` variant and classes

--- packages/ui-input/src/index.tsx
+++ packages/ui-input/src/index.tsx
@@ -5,13 +5,14 @@ import { merge } from "@halvaradop/ui-core"
 
 export type InputProps<T extends ArgsFunction> = Omit<ComponentProps<"input">, "size"> & VariantProps<T>
 
-export const inputVariants = cva("text-slate-600 border focus-within:outline-none ", {
+export const inputVariants = cva("text-slate-600 border focus-within:outline-none disabled:cursor-not-allowed disabled:text-gray-400 disabled:border-gray-300 disabled:bg-gray-100", {
     variants: {
         variant: {
             base: "caret-slate-600 border-gray-300",
             outline: "focus-within:ring-1 focus-within:border-slate-700 focus-within:ring-slate-700",
             required:
                 "focus-within:ring-1 focus-within:border-slate-700 focus-within:ring-slate-700 usinvalid:text-rose-300 usinvalid:placeholder-rose-400  usinvalid:border-rose-400 usinvalid:focus-within:ring-rose-400 usvalid:text-green-400 usvalid:border-green-400 usvalid:focus-within:ring-green-400 input-empty:border-slate-700 input-empty:focus-within:ring-slate-700 input-empty:caret-slate-600",
+            disabled: "cursor-not-allowed bg-gray-100 text-gray-400 border-gray-300",
         },
         size: {
             sm: "h-8 indent-2 text-sm rounded-md",
