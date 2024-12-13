8319655af99516157c4bef406e2ebccb10a8e140
Hernan Alvarado <hernanvid123@gmail.com>
2024-12-13 11:53:15 -0500

    feat: add `Checkbox` component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,6 +14,7 @@ const config: StorybookConfig = {
         "../stories/**/*.mdx",
         "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
         "../packages/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+        "!../packages/**/node_modules/**",
     ],
     addons: [
         getAbsolutePath("@storybook/addon-onboarding"),
