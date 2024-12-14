89ed95b25fefa2bdf6fec16dd68dfc7a453adc1c
Hernan Alvarado <hernanvid123@gmail.com>
2024-12-14 18:34:51 -0500

    fix: resolve issue in `cva` types to build packages

--- packages/ui-submit/src/index.tsx
+++ packages/ui-submit/src/index.tsx
@@ -1,15 +1,15 @@
 "use client"
 import { ComponentProps } from "react"
-import { useFormStatus } from "react-dom"
-import { type ArgsFunction, type VariantProps, cva } from "@halvaradop/ui-core"
+import { ArgsFunction, merge } from "@halvaradop/ui-core"
+import { cva, VariantProps } from "class-variance-authority"
 
-export type SubmitProps<T extends ArgsFunction> = Omit<ComponentProps<"input">, "type" | "size"> & VariantProps<T> & { pending?: string }
+export type SubmitProps<T extends ArgsFunction> = VariantProps<T> & Omit<ComponentProps<"input">, "type" | "size"> & { pending?: string }
 
 export const submitVariants = cva("font-medium border focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 disabled:hover:cursor-progress", {
     variants: {
         variant: {
             base: "text-white border-black bg-black ring-black disabled:bg-opacity-90",
-            inverted: "text-black border-white bg-white ring-white ring-offset-black disabled:border-black disabled:bg-opacity-90",
+            inverted: " inverted text-black border-white bg-white ring-white ring-offset-black disabled:border-black disabled:bg-opacity-90",
         },
         size: {
             sm: "h-9 px-3 text-sm rounded",
@@ -33,8 +33,8 @@ export const submitVariants = cva("font-medium border focus-within:outline-none 
     },
 })
 
-export const Submit = ({ className, variant, size, fullWidth, fullRounded, value = "Submit", pending = "Submitting...", ref, ...props }: SubmitProps<typeof submitVariants>) => {
-    const { pending: status } = useFormStatus()
+export const Submit = ({ className, value = "Submit", pending = "Submitting..." }: SubmitProps<typeof submitVariants>) => {
+    const { pending: status } = { pending: false }
     const message = status ? pending : value
-    return <input className={submitVariants({ className, variant, size, fullWidth, fullRounded })} type="submit" value={message} disabled={status} aria-disabled={status} ref={ref} {...props} />
+    return <input className={merge(submitVariants({ className }))} type="submit" value={message} disabled={status} aria-disabled={status} />
 }
