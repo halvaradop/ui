5e63f83e113ef8c95f5bb2b923ab283a9012f5bf
Hernan Alvarado <hernanvid123@gmail.com>
2025-01-27 17:08:04 -0500

    fix: resolve issue in `radio-group.stores`

--- packages/ui-radio-group/src/index.tsx
+++ packages/ui-radio-group/src/index.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { forwardRef, useEffect, useRef } from "react"
 import { merge, type ComponentProps, type WithChildrenProps, type ArgsFunction } from "@halvaradop/ui-core"
 import { cva, type VariantProps } from "class-variance-authority"
