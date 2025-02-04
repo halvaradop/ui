5aafa8d7796de32f13f98feea6ee4b449af12e49
Hernan Alvarado <hernanvid123@gmail.com>
2025-02-04 17:30:43 +0000

    feat(packages): add `ui-utils` package (#107)
    * feat(packages): add `ui-utils` package
    
    - Export `tsconfig.base.json` file
    - Export `tsup.config.base.ts`
    - Export `decorator.tsx` file used in stories
    
    * chore: update `build:core` script in `package.json`
    
    * fix: resolve redundancy in button usage
    
    * chore: clean up stories
    
    * chore(docs): update `README.md` files for `core` and `utils`packages
    
    * chore(docs): enhance description of `decorator.tsx` file

--- packages/ui-core/src/index.ts
+++ packages/ui-core/src/index.ts
@@ -1,7 +1,6 @@
 /**
  * @package @halvaradop/ui-core
  */
-export * from "./tsup.config.base.js"
 export * from "./slot.js"
 export * from "./utils.js"
 export type * from "./types.js"
---
+++ packages/ui-utils/src/decorator.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+import { Button } from "@halvaradop/ui-button"
+import type { Decorator } from "@storybook/react"
+
+export const decorator: Decorator = (Story) => {
+    const [isDark, setIsDark] = useState(false)
+
+    const handleToggleTheme = () => {
+        setIsDark((previous) => !previous)
+        document.querySelector("html")?.classList.toggle("dark", !isDark)
+    }
+
+    useEffect(() => {
+        setIsDark(document.querySelector("html")?.classList?.contains("dark") ?? false)
+    }, [])
+
+    return (
+        <div
+            className="w-full h-full mx-auto flex items-center justify-center absolute inset-0 data-[dark='true']:bg-black"
+            data-dark={isDark}
+        >
+            <div className="w-full dark:text-white">
+                <Button className="absolute top-[4%] left-[4%]" onClick={handleToggleTheme}>
+                    Theme
+                </Button>
+                <section className="story-container">
+                    <Story />
+                </section>
+            </div>
+        </div>
+    )
+}
