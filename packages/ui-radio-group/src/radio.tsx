0ce4c9ff9b1811de31a8d1b6b124e447736e0d30
Hernan Alvarado <hernanvid123@gmail.com>
2025-03-12 14:02:58 -0500

    fix(radio-group): re-write `Radio` component

--- packages/ui-radio-group/src/radio.tsx
+++ packages/ui-radio-group/src/radio.tsx
@@ -1,7 +1,16 @@
-import { Radio as RadioComponent, radioVariants } from "@halvaradop/ui-radio"
+import { Radio as RadioComponent, radioVariants, type RadioProps as RadioBaseProps } from "@halvaradop/ui-radio"
 import { useRadioGroup } from "./context.js"
 
-export const Radio = ({ value, ...props }: any) => {
+export const Radio = ({ className, size, color, value, ...props }: RadioBaseProps<typeof radioVariants>) => {
     const { name, selectedValue, onChange } = useRadioGroup()
-    return <RadioComponent name={name} value={value} onChange={onChange} checked={selectedValue === value} {...props} />
+    return (
+        <RadioComponent
+            className={radioVariants({ className, size, color })}
+            name={name}
+            value={value}
+            onChange={onChange}
+            checked={selectedValue === value}
+            {...props}
+        />
+    )
 }
