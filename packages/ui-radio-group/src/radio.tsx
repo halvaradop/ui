ce251635115e213c2f2b9da215c950fe079cf31f
Hernan Alvarado <hernanvid123@gmail.com>
2025-03-27 19:11:10 -0500

    feat(source-maps): set up `source-maps` in `tsup` and `typescript`
    - Rename `build:radio-group` to `build:unstable`
    - Add `build:release` script
    - Rename `build-storybook` to `build:storybook`

---
+++ packages/ui-radio-group/src/radio.tsx
@@ -0,0 +1,19 @@
+import { Radio as DefaultRadio, radioVariants, type RadioProps } from "@halvaradop/ui-radio"
+import { useRadioGroup } from "./context.js"
+
+const Radio = ({ name, value, ref, ...props }: Omit<RadioProps<typeof radioVariants>, "onChange">) => {
+    const { name: defaultName, selectedValue, onChange } = useRadioGroup()
+
+    return (
+        <DefaultRadio
+            ref={ref}
+            name={name ?? defaultName}
+            value={value}
+            checked={selectedValue === value}
+            onChange={onChange}
+            {...props}
+        />
+    )
+}
+
+export { Radio, radioVariants }
