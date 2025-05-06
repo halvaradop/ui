b2986b21f124eae3ef7b35722b90e18b2e1cd26f
Hernan Alvarado <hernanvid123@gmail.com>
2025-05-06 11:56:45 -0500

    chore(radio-group): update logic

--- packages/ui-radio-group/src/radio.tsx
+++ packages/ui-radio-group/src/radio.tsx
@@ -1,7 +1,12 @@
 import { Radio as DefaultRadio, radioVariants, type RadioProps } from "@halvaradop/ui-radio"
 import { useRadioGroup } from "./context.js"
 
-const Radio = ({ name, value, ref, ...props }: Omit<RadioProps<typeof radioVariants>, "onChange">) => {
+const Radio = ({
+    name,
+    value,
+    ref,
+    ...props
+}: Omit<RadioProps<typeof radioVariants>, "value" | "onChange"> & { value: string }) => {
     const { name: defaultName, selectedValue, onChange } = useRadioGroup()
 
     return (
@@ -10,7 +15,7 @@ const Radio = ({ name, value, ref, ...props }: Omit<RadioProps<typeof radioVaria
             name={name ?? defaultName}
             value={value}
             checked={selectedValue === value}
-            onChange={onChange}
+            onChange={() => onChange?.(value)}
             {...props}
         />
     )
