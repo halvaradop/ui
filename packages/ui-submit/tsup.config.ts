c4e819e7dd2e76bceae65e12909d73a040f8d1d6
Hernan Alvarado <hernanvid123@gmail.com>
2025-05-11 21:15:50 -0500

    chore(config): add alias imports

--- packages/ui-submit/tsup.config.ts
+++ packages/ui-submit/tsup.config.ts
@@ -1,8 +1,4 @@
 import { defineConfig } from "tsup"
 import { tsupConfig } from "@halvaradop/ui-utils/tsup.config.base"
 
-export default defineConfig((options) => ({
-    ...tsupConfig,
-    minify: !options.watch,
-    splitting: !options.watch,
-}))
+export default defineConfig(tsupConfig)
