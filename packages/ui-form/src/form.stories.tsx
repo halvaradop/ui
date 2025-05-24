6c50fff0b406abfec4938026e5898c4aefe296d6
Hernan Alvarado <hernanvid123@gmail.com>
2025-05-24 16:06:29 -0500

    [SYNC] test(storybook): add play functions in storybook (#159)

--- packages/ui-form/src/form.stories.tsx
+++ packages/ui-form/src/form.stories.tsx
@@ -1,7 +1,10 @@
+import { ChangeEvent, FormEvent, useState } from "react"
 import type { Meta, StoryObj } from "@storybook/react"
+import { expect, userEvent, within } from "@storybook/test"
 import { Form } from "./index.js"
 import { Input } from "@/ui/ui-input/src/index.js"
 import { Label } from "@/ui/ui-label/src/index.js"
+import { Submit } from "@/ui/ui-submit/src/index.js"
 import { decorator } from "@halvaradop/ui-utils/decorator"
 import { DocsPage } from "@halvaradop/ui-utils/docs-page"
 
@@ -56,18 +59,42 @@ export const Base: Story = {
     parameters: {
         skipDecorator: true,
     },
-    render: ({ size, variant }) => (
-        <Form size={size} variant={variant}>
-            <Label>
-                Username
-                <Input />
-            </Label>
-            <Label>
-                Password
-                <Input />
-            </Label>
-        </Form>
-    ),
+    render: ({ size, variant }) => {
+        const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+            event.preventDefault()
+        }
+
+        return (
+            <Form size={size} variant={variant} onSubmit={handleSubmit}>
+                <Label htmlFor="username">
+                    Username
+                    <Input name="username" id="username" placeholder="johndoe" />
+                </Label>
+                <Label htmlFor="password">
+                    Password
+                    <Input name="password" id="password" type="password" placeholder="******" />
+                </Label>
+                <Submit className="mt-2" fullWidth value="Login" />
+            </Form>
+        )
+    },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement)
+        const username = canvas.getByPlaceholderText("johndoe")
+        await userEvent.type(username, "halvaradop", { delay: 102 })
+        await expect(username).toHaveValue("halvaradop")
+
+        const password = canvas.getByPlaceholderText("******")
+        await userEvent.type(password, "halvaradop", { delay: 102 })
+        await expect(password).toHaveValue("halvaradop")
+
+        await userEvent.click(canvas.getByText("Login"))
+
+        await userEvent.clear(username)
+        await userEvent.clear(password)
+        await expect(username).toHaveValue("")
+        await expect(password).toHaveValue("")
+    },
 }
 
 export default meta
--- packages/ui-submit/src/submit.stories.tsx
+++ packages/ui-submit/src/submit.stories.tsx
@@ -1,5 +1,5 @@
-"use client"
 import type { ArgTypes, Meta, StoryObj } from "@storybook/react"
+import { expect, getAllByRole, userEvent, within } from "@storybook/test"
 import { Submit } from "./index.js"
 import { decorator } from "@halvaradop/ui-utils/decorator"
 import { DocsPage } from "@halvaradop/ui-utils/docs-page"
@@ -67,6 +67,18 @@ const meta: Meta = {
         },
     },
     decorators: [decorator],
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement)
+        const buttons = canvas.getAllByRole("button")
+        for (const button of buttons.slice(1)) {
+            await expect(button).toBeInTheDocument()
+            button.setAttribute("disabled", "true")
+            await expect(button).toBeDisabled()
+            await new Promise((resolve) => setTimeout(resolve, 500))
+            button.removeAttribute("disabled")
+            await expect(button).not.toBeDisabled()
+        }
+    },
 } satisfies Meta<typeof Submit>
 
 type Story = StoryObj<typeof meta>
