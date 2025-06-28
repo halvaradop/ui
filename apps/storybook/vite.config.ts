a15cf0bd105f2167944c1fc78053a06e5ffce257
Hernan Alvarado <hernanvid123@gmail.com>
2025-06-28 16:44:40 -0500

    chore(structure): create apps/storybook directory to align with Turborepo standards

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
-import { tailwindcss } from "@halvaradop/tailwind-config/tailwind-vite"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
