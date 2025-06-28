39ea36a4a6a677705bc6c72e2388435e41cb85b2
Hernan Alvarado <hernanvid123@gmail.com>
2025-06-28 17:46:08 -0500

    ci(scripts): exclude Storybook from component build scripts

--- packages/ui-utils/tsup.config.ts
+++ packages/ui-utils/tsup.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "tsup"
 import { tsupConfig } from "./src/tsup.config.base.js"
 
-export default defineConfig(tsupConfig)
+export default defineConfig((options) => {
+    console.log("options: ", options)
+    return { ...tsupConfig, ...options }
+})
