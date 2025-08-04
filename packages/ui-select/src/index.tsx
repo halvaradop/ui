ed6cd648af69f9557439bb39714d82a137eb68b9
Hernan Alvarado <hernanvid123@gmail.com>
2025-08-04 11:59:21 -0500

    feat(select): implement a `Dot Notation` pattern

--- packages/ui-select/src/index.tsx
+++ packages/ui-select/src/index.tsx
@@ -1,3 +1,20 @@
+import { Select as SelectRoot, type SelectProps } from "./select.js"
+import { SelectList } from "./list.js"
+import { SelectOption } from "./option.js"
+import { SelectTrigger } from "./trigger.js"
+
+interface SelectComponent extends React.FC<SelectProps> {
+    List: typeof SelectList
+    Option: typeof SelectOption
+    Trigger: typeof SelectTrigger
+}
+
+const Select = SelectRoot as SelectComponent
+
+Select.List = SelectList
+Select.Option = SelectOption
+Select.Trigger = SelectTrigger
+
 export * from "./context.js"
 export * from "./select.js"
 export * from "./trigger.js"
