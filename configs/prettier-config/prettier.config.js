f47f9a253a6c8e305739a70065993582e0b3eaac
Hernan Alvarado <hernanvid123@gmail.com>
2025-08-04 11:19:20 -0500

    chore(prettier): add `.prettierignore` to exclude patterns

--- configs/prettier-config/prettier.config.js
+++ configs/prettier-config/prettier.config.js
@@ -2,16 +2,18 @@
  * @type {import("prettier").Config}
  */
 export const config = {
-  semi: false,
-  tabWidth: 4,
-  printWidth: 120,
-  trailingComma: "es5",
-  overrides: [
-    {
-      files: ["*.json", "*.md", "*.yaml", "*.yml"],
-      options: {
-        tabWidth: 2,
-      },
-    },
-  ],
-};
+    semi: false,
+    tabWidth: 4,
+    printWidth: 120,
+    trailingComma: "es5",
+    overrides: [
+        {
+            files: ["*.json", "*.md", "*.yaml", "*.yml"],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+}
+
+export default config
