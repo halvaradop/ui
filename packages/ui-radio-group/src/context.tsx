ebbd8307ad638c84f13d23ec590032d52975124f
Hernan Alvarado <hernanvid123@gmail.com>
2025-12-29 18:41:39 -0500

    chore: run format

--- packages/ui-radio-group/src/context.tsx
+++ packages/ui-radio-group/src/context.tsx
@@ -1,8 +1,9 @@
 import { createContext, useCallback, useContext, useId, useMemo, useState, useEffect } from "react"
 import type { RadioGroupProps, radioGroupVariants } from "./radio-group.js"
 
-export interface RadioGroupContextType
-    extends Required<Pick<RadioGroupProps<typeof radioGroupVariants>, "name" | "value">> {
+export interface RadioGroupContextType extends Required<
+    Pick<RadioGroupProps<typeof radioGroupVariants>, "name" | "value">
+> {
     id: string
     onValueChange: (value: string) => void
 }
